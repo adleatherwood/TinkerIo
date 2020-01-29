e2bf081ad6a6f8ebfdd37a131206dd7651f0a98e
adleatherwood <adleatherwood@gmail.com>
2020-01-28 21:25:11 -0500

    feat (stream): added long poll to stream read

--- src/Stream/StreamController.fs
+++ src/Stream/StreamController.fs
@@ -12,11 +12,21 @@ type StreamController() =
     [<HttpPost("append/{stream}")>]
     member __.Append(stream: string, [<FromBody>] content: JRaw) = async {
         let! result = Stream.append stream (content.ToString())
+
+        StreamLock.Signal stream
+
         return StreamConvert.toJson result
     }
 
     [<HttpGet("read/{stream}/{offset}/{max}")>]
     member __.Read(stream: string, offset: uint32, max: uint32) = async {
-        let! result = Stream.read stream offset max
+        let! result = StreamLock.Wait stream (fun () ->
+            async {
+                let! r = Stream.read stream offset max
+                if r.Entries.Length = 0
+                then return None
+                else return Some r
+            })
+
         return result
     }
---
+++ src/Stream/StreamLock.fs
@@ -0,0 +1,31 @@
+namespace TinkerIo.Stream
+
+open System
+open System.Threading
+
+module StreamLock =
+
+    let maxPartitions = 100
+
+    let private partitions =
+        [0 .. maxPartitions]
+        |> List.map (fun i -> (i, new ManualResetEventSlim()))
+        |> dict
+
+    let private indexOf(key: string) =
+        key.GetHashCode() % maxPartitions
+        |> Math.Abs
+
+    let rec Wait(key: string) (f: unit -> Async<'a option>) : Async<'a> =  async {
+        match! f() with
+        | Some result -> return result
+        | _ ->
+            let i = indexOf(key)
+            partitions.[i].Reset()
+            partitions.[i].Wait()
+            return! Wait key f
+        }
+
+    let Signal(key: string) =
+        let i = indexOf key
+        partitions.[i].Set()
